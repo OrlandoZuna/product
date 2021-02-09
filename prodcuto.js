4b7543fc4e9039366e725e083d0d43239d973e1d
orlando <zunaorlando10@gmail.com>
2021-02-09 01:50:32 -0300

    miprimer committ

---
+++ prodcuto.js
@@ -0,0 +1,36 @@
+class productos{
+    constructor(name, description, quantity){
+         this.name = name;
+         this.description= description;
+         this.quantity=quantity;
+    }
+}
+class productosManagement {
+    constructor() {
+        this.listproductos = []
+    }
+    addproductos(producto){
+        this.listproductos.push(producto);
+    }
+    removeproductos(producto){
+        for (var i=0; i< this.listproductos.length; i++){
+            if(this.listproductos[i]== producto){
+                this.listproductos.splice(i, 1);
+                return;
+            }
+        }
+        return;
+    }
+    updateproductos(producto, newproducto){
+        for (var i=0; i< this.listproductos.length; i++){
+            if(this.listproductos[i]== producto){
+                this.listproductos[i]= newproducto;
+                return;
+            } 
+        }
+    }
+    showproductos(){
+        return this.listproductos;
+    }
+}
+export{productos, productosManagement};
